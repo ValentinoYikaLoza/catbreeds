495219e09ee3d6b83824e69c9e4b11e28a5dd711
ValentinoYikaLoza <138173621+ValentinoYikaLoza@users.noreply.github.com>
2024-11-28 18:46:14 -0500

    ADD: inicio de proyecto

---
+++ catbreeds/README.md
@@ -0,0 +1,7 @@
+# catbreeds
+
+A new Flutter project.
+
+add .env file with this content:
+
+- URL_BASE=https://api.thecatapi.com/v1/breeds
---
+++ catbreeds/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:catbreeds/app/app.dart';
+import 'package:catbreeds/app/config/constants/environment.dart';
+import 'package:catbreeds/app/config/router/app_router.dart';
+import 'package:catbreeds/app/config/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() async {
+  await Environment.initEnvironment();
+
+  runApp(const ProviderScope(child: MainApp()));
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.getTheme(),
+      builder: (context, child) {
+        return App(
+          child: child!,
+        );
+      },
+    );
+  }
+}
